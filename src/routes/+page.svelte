a237e2fa0a916954a94f6c589dae5df8a2830c9e
iJustGhost <fryingchicken81@gmail.com>
2026-04-22 07:44:23 +0800

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    let name = $state();
+    let price = $state();
+    let ip = $state();
+
+    async function updateTag() {
+        await fetch(`http://${ip}/update?name=${name}&price=${price}`);
+    }
+
+</script>
+
+<input type="text" name="name" id="" bind:value={name}>
+<input type="text" name="price" id="" bind:value={price}>
+<input type="text" name="ip" id="" bind:value={ip}>
+<button onclick={updateTag}>Submit</button>
